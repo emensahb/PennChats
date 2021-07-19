c6ee3818b5850452a0c4ab0e156be3e5b256afdd
jli293 <84085953+jli293@users.noreply.github.com>
2021-07-19 12:01:48 -0400

    July 19 AM Checkpoint

--- pennchatsproject/models.py
+++ pennchatsproject/models.py
@@ -14,15 +14,13 @@ class Student(db.Model, UserMixin):
     nullable=False means it will be compulsory to set the value for that column.
     index=True indicates that the column will be indexed.
     There is a many to many relationship between student and current courses,
-    past courses, interests, and groups."""
+    past courses, interests, and meetings."""
 
     __tablename__ = 'students'
 
-    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, primary_key=True, unique=True)
     email = db.Column(db.String(64), index=True, unique=True, nullable=False)
     username = db.Column(db.String(64), unique=True, index=True)
-    # not sure if we can just use student_id as primary key here.
-    student_id = db.Column(db.Integer, unique=True)
     password_hash = db.Column(db.String(128))
     firstname = db.Column(db.Text)
     lastname = db.Column(db.Text)
@@ -42,14 +40,16 @@ class Student(db.Model, UserMixin):
         'Course', secondary='past_courses_record', backref='past_students')
     interests = db.relationship(
         'Interest', secondary='student_interest_record', backref='all_students')
+    meetings = db.relationship(
+        'Meeting', secondary='groupings', backref='student')
 
     # many to one relationships
-    # calling weekly_signups.student will refer to the student associated with the weekly signup form
     weekly_signups = db.relationship('WeeklySignUp', backref='student')
+    # calling weekly_signups.student will refer to the student associated with the weekly signup form
 
     # one to many relatinoships
     course_id_to_match = db.Column(db.Integer, db.ForeignKey(
-        'courses.id'))  # one to many, use course table
+        'courses.course_id'))  # one to many, use course table
     interest_id_to_match = db.Column(db.Integer, db.ForeignKey(
         'interests.interest_id'))  # one to many, use interest table twice
 
@@ -78,7 +78,7 @@ class WeeklySignUp(db.Model):
 
     # one to many relationships
     student_id = db.Column(db.Integer, db.ForeignKey(
-        'students.id'), nullable=False)
+        'students.student_id'), nullable=False)
 
     prime_time_id = db.Column(db.Integer, db.ForeignKey(
         'time_options.time_id'), nullable=False)
@@ -111,12 +111,10 @@ class Course(db.Model):
 
     __tablename__ = 'courses'
 
-    id = db.Column(db.Integer, primary_key=True)
-    # not sure if we can just use course_id as primary key here
-    course_id = db.Column(db.Text, unique=True, nullable=False)
+    course_id = db.Column(db.Text, primary_key=True, unique=True, nullable=False)
     course_name = db.Column(db.Text, nullable=False)
-    # calling student.course_to_match will return all the course this student prefers to be matched with
     students = db.relationship('Student', backref='course_to_match')
+    # calling student.course_to_match will return the course this student prefers to be matched with
 
     def __init__(self, course_id, course_name):
         self.course_id = course_id
@@ -125,19 +123,19 @@ class Course(db.Model):
     def __repr__(self):
         return f"This is the course of {self.course_name} with the course ID of {self.course_id}."
 
-
+# many to many relatinoship association tables
 current_courses_record = db.Table('current_courses_record',
                                   db.Column('student_id', db.Integer, db.ForeignKey(
-                                      'students.id'), primary_key=True),
+                                      'students.student_id'), primary_key=True),
                                   db.Column('course_id', db.Integer, db.ForeignKey(
-                                      'courses.id'), primary_key=True)
+                                      'courses.course_id'), primary_key=True)
                                   )
 
 past_courses_record = db.Table('past_courses_record',
                                db.Column('student_id', db.Integer, db.ForeignKey(
-                                   'students.id'), primary_key=True),
+                                   'students.student_id'), primary_key=True),
                                db.Column('course_id', db.Integer, db.ForeignKey(
-                                   'courses.id'), primary_key=True)
+                                   'courses.course_id'), primary_key=True)
                                )
 
 
@@ -145,14 +143,13 @@ class Interest(db.Model):
     """This table is used to store all interests we plan to provide as options for students to choose from
     when they fill out their user profile.
     There is a many to many relationship between this table and the student table."""
-    # how do we input data to this table?
 
     __tablename__ = 'interests'
 
     interest_id = db.Column(db.Integer, primary_key=True)
     interest_name = db.Column(db.Text, nullable=False, unique=True)
-    # calling student.interest_to_match will return all the interest this student prefers to be matched with
     students = db.relationship('Student', backref='interest_to_match')
+    # calling student.interest_to_match will return all the interest this student prefers to be matched with
 
     def __init__(self, interest_name):
         self.interest_name = interest_name
@@ -160,10 +157,10 @@ class Interest(db.Model):
     def __repr__(self):
         return f"This is the interest of {self.interest_name} with the interest ID of {self.interest_id}."
 
-
+# many to many association table
 student_interest_record = db.Table('student_interest_record',
                                    db.Column('student_id', db.Integer, db.ForeignKey(
-                                       'students.id'), primary_key=True),
+                                       'students.student_id'), primary_key=True),
                                    db.Column('interest_id', db.Integer, db.ForeignKey(
                                        'interests.interest_id'), primary_key=True)
                                    )
@@ -220,3 +217,31 @@ class NetworkingGoal(db.Model):
 
     def __repr__(self):
         return f"This is the networking goal of {self.networking_goal} with the ID of {self.networking_goal_id}."
+
+
+class Meeting(db.Model):
+    """This table describes all the meetings that has been set up.
+    The final output of the matching algorithm will be a list of Meeting objects.
+    There is a many-to-many relationship between this table and the Student table."""
+    # how do we input data to this table?
+
+    __tablename__ = 'meetings'
+
+    meeting_id = db.Column(db.Integer, primary_key=True)
+    time_id = db.Column(db.Integer)
+    course_id = db.Column(db.Integer)
+    interest_id = db.Column(db.Integer)
+
+    def __init__(self, time_id, course_id=None, interest_id=None):
+        self.group = group
+        self.time_id = time_id # could potentially add one to many relationship
+        self.course_id = course_id
+        self.interest_id = interest_id
+
+# many to many association table
+groupings = db.Table('groupings',
+                     db.Column('student_id', db.Integer, db.ForeignKey(
+                         'students.student_id'), primary_key=True),
+                     db.Column('meeting_id', db.Integer, db.ForeignKey(
+                         'meetings.meeting_id'), primary_key=True)
+                     )
--- pennchatsproject/students/views.py
+++ pennchatsproject/students/views.py
@@ -1,22 +1,21 @@
 # pennchatsproject/students/views.py
 # contains routing code for eight views
 # register, login, logout, sign-up for next week's chat, thank you, account, edit profile, <username> profile page
-# from Audra's main.py file lines 12-44
 
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, login_required, logout_user, current_user
 from pennchatsproject import db
-#from werkzeug.security import generate_password_hash, check_password_hash
-from pennchatsproject.models import Student, WeeklySignUp
-from pennchatsproject.students.forms import RegistrationForm, LoginForm
-from pennchatsproject.students.picture_handler import add_profile_pic
+# from werkzeug.security import generate_password_hash, check_password_hash
+from pennchatsproject.models import *
+from pennchatsproject.students.forms import *
+from pennchatsproject.setup import *
+# from pennchatsproject.students.picture_handler import add_profile_pic
 
 
 students = Blueprint('students', __name__)
 
+
 #register
-
-
 @students.route('/register', methods=["POST", "GET"])
 def register():
     form = RegistrationForm()
@@ -35,36 +34,28 @@ def register():
     return render_template("register.html", form=form)
 
 #login
-
-
 @students.route('/login', methods=["POST", "GET"])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         # Grab the student from our Student Models table
         student = Student.query.filter_by(email=form.email.data).first()
 
         if student.check_password(form.password.data) and student is not None:
-
             #log in the user by calling in the login_user method
             login_user(student)
-
             #flash a msg with the following message
             flash('You have logged in successfully!')
-
             # if a user was trying to visit a page that requires a log, flask saves that URL as 'next'
             next = request.args.get('next')
-
             if next == None or not next[0] == '/':
                 next = url_for('core.index')
-
             return redirect(next)
 
     return render_template("login.html", form=form)
 
 #logout
-
-
 @students.route('/logout')
 @login_required
 def logout():
@@ -72,38 +63,71 @@ def logout():
     flash("You have successfully logged out.")
     return redirect(url_for('core.index'))
 
-# #sign up-for next week's chat
+# update profile page
+@students.route('/edit_profile', methods=["POST", "GET"])
+@login_required
+def edit_profile():
+    form = ProfileForm()
 
-
-@students.route('/next_week')
-def next_week():
-    form = NextWeekForm()
     if form.validate_on_submit():
+        # print(form)
+
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.city = form.city.data
+        current_user.state = form.state.data
+        current_user.country = form.country.data
+        current_user.linkedin = form.linkedin.data
+        current_user.bio = form.bio.data
+        current_user.current_courses = form.current_courses.data
+        current_user.past_courses = form.past_courses.data
+        current_user.course_id_to_match = form.course_id_to_match.data
+        current_user.interests = form.interests.data
+        current_user.interest_id_to_match = form.interest_id_to_match.data
+        current_user.cohort = form.cohort.data
+
+        db.session.commit()
+        flash('Profile Updated')
+        return redirect(url_for('students.account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    return render_template('edit_profile.html', form=form)
+
+# sign up-for next week's chat
+@students.route('/sign_up', methods=["POST", "GET"])
+@login_required
+def sign_up():
+    form = WeeklySignUpForm()
+
+    if form.validate_on_submit():
+
         form = WeeklySignUp(week_meet=form.week_meet.data,
-                            prime_time=form.prime_time.data,
-                            sec_time=form.sec_time.data,
-                            networking_goal=form.networking_goal.data)
+                            student_id=current_user.student_id,
+                            prime_time_id=form.prime_time_id.data,
+                            sec_time_id=form.sec_time_id.data,
+                            prime_networking_goal_id=form.prime_networking_goal_id.data,
+                            sec_networking_goal_id=form.sec_networking_goal_id.data,
+                            )
 
         db.session.add(form)
         db.session.commit()
+        flash('Signup Form Submitted')
         return redirect(url_for('students.thank_you'))
 
-    return render_template("next_week.html", form=form)
-#
-# #thank you
-# @students.route('/thank_you')
-# def thank_you():
-#     return 'Thanks for signing up!'
+    return render_template("weekly_signup.html", form=form)
+
+#thank you
+@students.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
 #
 # #account
 # @students.route('/account')
 # def member_area():
 #     return render_template("account.html")
-#
-# #edit profile
-# @students.route('/account/<name>')
-# def edit_profile(name):
-#     return 'edit profile'
 #
 # #<username> profile page
 # @students.route("/name/<name>")
