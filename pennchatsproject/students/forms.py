818f7064350ef23abbe63eb7847c4ab9dcf3d235
jli293 <84085953+jli293@users.noreply.github.com>
2021-07-25 19:23:45 -0400

    Connected Audra's frontend changes. Forms look nice now!

--- pennchatsproject/students/forms.py
+++ pennchatsproject/students/forms.py
@@ -20,93 +20,6 @@ def interest_query():
 def course_query():
     return Course.query
 
-class MultipleCheckboxField(SelectMultipleField):
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
-
-class ProfileForm(FlaskForm):
-
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    city = StringField('City', validators=[DataRequired()])
-    state = StringField('State/Province', validators=[DataRequired()])
-    country = StringField('Country/Territory', validators=[DataRequired()])
-    linkedin = StringField('LinkedIn', validators=[URL()])
-    bio = TextAreaField('Brief Bio (optional)')
-
-    # Class
-    class_list = ['CIT591: Introduction to Software Development',
-                  'CIT592: Mathematical Foundations of CS',
-                  'CIT593: Introduction to Computer Systems',
-                  'CIT594: Data Structures & Software Design',
-                  'CIT595: Computer Systems Programming',
-                  'CIT596: Algorithms & Computation',
-                  'CIS515: Math for Machine Learning',
-                  'CIS521: Artificial Intelligence',
-                  'CIS547: Software Analysis',
-                  'CIS549: Wireless Communications',
-                  'CIS550: Database & Information Systems',
-                  'CIS581: Computer Vision & Photography',
-                  'CIS520: Intro to Robotics',
-                  'CIS582: Blockchains & Cryptography',
-                  'ESE542: Statistics for Data Science']
-    # Gets the list of courses from the DB
-    # class_list = [x.course_id for x in Course.query.all()]
-    # create value/label pairs (should both be str for name of class)
-    class_tuples = [(x, x) for x in class_list]
-    # current courses
-    current_courses = MultipleCheckboxField(
-        'Current Courses', choices=class_tuples)
-    # past courses
-    past_courses = MultipleCheckboxField('Past Courses', choices=class_tuples)
-    # preferred course to be matched on
-    course_id_to_match = SelectField(
-        'Preferred course to be matched with',
-        validators=[DataRequired()],
-        choices=class_tuples,
-    )
-
-    # Interest
-    interest_name_list = ['Artificial Intelligence & Machine Learning', 'Blockchain', 'Cybersecurity & Cryptography',
-    'Data Science', 'Game Design', 'Interview Prep', 'Mathematics for Computer Science',
-    'Networking & Computer Systems', 'Project Management', 'Software Development']
-    interest_id_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    # interest_id_list = [x.interest_id for x in Interest.query.all()]
-    # interest_name_list = [y.interest_name for y in Interest.query.all()]
-    # create value/label pairs (should both be str for name of interest)
-    interest_tuples = list(
-        map(lambda x, y: (x, y), interest_id_list, interest_name_list))
-    interests = MultipleCheckboxField('Interests', choices=interest_tuples)
-    # preferred interest to be matched on
-    interest_id_to_match = SelectField(
-        'Preferred interest to be matched with',
-        validators=[DataRequired()],
-        choices=interest_tuples,
-    )
-
-    # Cohort
-    cohort_list=[
-    'Spring 2018',
-    'Fall 2018',
-    'Spring 2019',
-    'Fall 2019',
-    'Spring 2020',
-    'Fall 2020',
-    'Spring 2021',
-    'Fall 2021'
-    ]
-    cohort_id_list = [1, 2, 3, 4, 5, 6, 7, 8]
-    # Gets the list of courses from the DB
-    # cohort_list = [x.cohort_name for x in Cohort.query.all()]
-    # create value/label pairs (should both be str for name of cohort)
-    cohort_tuples = list(
-            map(lambda x, y: (x, y), cohort_id_list, cohort_list))
-    cohort = MultipleCheckboxField('MCIT Cohort', choices=cohort_tuples)
-
-    # Submit
-    submit = SubmitField('Update Profile')
-
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -152,6 +65,7 @@ class ProfileForm(FlaskForm):
     state = StringField('State/Province')
     country = StringField('Country/Territory')
     linkedin = StringField('LinkedIn')
+    # linkedin = StringField('LinkedIn', validators=[URL()])
     bio = TextAreaField('Brief Bio (optional)')
 
     # Course
@@ -159,9 +73,9 @@ class ProfileForm(FlaskForm):
     class_list = [x.course_id for x in Course.query.all()]
     class_tuples = [(x, x) for x in class_list]
     current_courses = QuerySelectMultipleField(
-        query_factory=course_query, allow_blank=True, get_label='course_id')
+        query_factory=course_query, allow_blank=True)
     past_courses = QuerySelectMultipleField(
-        query_factory=course_query, allow_blank=True, get_label='course_id')
+        query_factory=course_query, allow_blank=True)
     course_id_to_match = SelectField(
         'Preferred course to be matched with',
         validators=[DataRequired()],
@@ -171,7 +85,6 @@ class ProfileForm(FlaskForm):
     # Interest
     interest_id_list = [x.interest_id for x in Interest.query.all()]
     interest_name_list = [y.interest_name for y in Interest.query.all()]
-    # create value/label pairs (should both be str for name of interest)
     interest_tuples = list(
         map(lambda x, y: (x, y), interest_id_list, interest_name_list))
     interests = QuerySelectMultipleField(
@@ -186,8 +99,6 @@ class ProfileForm(FlaskForm):
     cohort_list = [x.cohort_name for x in Cohort.query.all()]
     cohort_tuples = [(x, x) for x in cohort_list]
     cohort = SelectField('MCIT Cohort', choices=cohort_tuples)
-    # cohort = QuerySelectField(
-    #     query_factory=cohort_query, allow_blank=True, get_label='cohort_name')
 
     # Submit
     submit = SubmitField('Update Profile')
@@ -195,14 +106,9 @@ class ProfileForm(FlaskForm):
 
 class WeeklySignUpForm(FlaskForm):
 
-    # week_meet list (hard-code for now)
-    # week_meet_list = ['Aug 2', 'Aug 9', 'Aug 16',
-    #                   'Aug 23', 'Aug 30', 'Sept 6', 'Sept 13',
-    #                   'Sept 20', 'Sept 27']
+    # week_meet list
     week_meet_list = [x.week_meet_name for x in WeekMeet.query.all()]
-    # create value/label pairs (should both be str for week_meet)
     week_meet_tuples = [(x, x) for x in week_meet_list]
-    # week_meet = DateTimeField('Week Meet', format='%Y-%m-%dT%H:%M:%S', default=datetime.today, validators=[DataRequired()])
     week_meet = SelectField(
         'Which week you would like to meet for PennChats? (Please do not submit more than one form for the same week)',
         validators=[DataRequired()],
@@ -210,12 +116,8 @@ class WeeklySignUpForm(FlaskForm):
     )
 
     # Prime Time & Sec Time
-    # time_id_list = [1, 2, 3, 4]
-    # time_option_list = ['Morning: 9am ET', 'Afternoon: 3pm ET', 'Evening: 7pm ET',
-    #                     'Overnight: 1am ET']
     time_id_list = [x.time_id for x in TimeOption.query.all()]
     time_option_list = [y.time_option for y in TimeOption.query.all()]
-    # create value/label pairs (should both be str for name of interest)
     time_tuples = list(
         map(lambda x, y: (x, y), time_id_list, time_option_list))
     prime_time_id = SelectField(
@@ -230,11 +132,8 @@ class WeeklySignUpForm(FlaskForm):
     )
 
     # Prime Goal & Sec Goal
-    # goal_id_list = [1, 2]
-    # goal_name_list = ['Match by Course', 'Match by Interest']
     goal_id_list = [x.networking_goal_id for x in NetworkingGoal.query.all()]
     goal_name_list = [y.networking_goal for y in NetworkingGoal.query.all()]
-    # create value/label pairs (should both be str for name of goal)
     goal_tuples = list(map(lambda x, y: (x, y), goal_id_list, goal_name_list))
     prime_networking_goal_id = SelectField(
         'Networking Goal',
