b5ffcd93ee0e79525804dbc0429505b0ddca9d8d
jli293 <84085953+jli293@users.noreply.github.com>
2021-07-27 13:09:39 -0400

    Added jinja contextfilter for displaying proper time in results tables

--- pennchatsproject/core/views.py
+++ pennchatsproject/core/views.py
@@ -2,9 +2,13 @@
 # includes routing code for two views (homepage view and the about us view)
 # Audra's main.py line 7-10 (Audra)
 
-from flask import render_template, request, Blueprint
+import jinja2
+from flask import render_template, Blueprint
+from pennchatsproject.models import TimeOption
+
 
 core = Blueprint('core', __name__)
+
 
 # home page
 @core.route('/')
@@ -12,7 +16,15 @@ def index():
     # more to come
     return render_template("index.html")
 
+
 # about us page
 @core.route('/info')
 def info():
     return render_template('info.html')
+
+
+@jinja2.contextfilter
+@core.app_template_filter()
+def query_time(context, time_id):
+    time = TimeOption.query.get(time_id)
+    return time.time_option
