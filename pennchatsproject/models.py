12cd5faa6d4a8a4fd4cee7b3fc824cea520a91cd
Efua Mensah-Brown <49411747+efbelle@users.noreply.github.com>
2021-07-27 20:54:34 -0500

    models update

--- pennchatsproject/models.py
+++ pennchatsproject/models.py
@@ -344,7 +344,7 @@ class UnmatchedStudents(db.Model):
     def __repr__(self):
         return f"Unmatched student: {self.first_name} {self.last_name}, {self.student_id}, and {self.email}. Unmatched week: {self.meeting_week_name}"
 
-# db.create_all()
+db.create_all()
 
 # time options
 t1 = TimeOption("Morning: 9am ET")
@@ -410,8 +410,8 @@ co5 = Cohort("Spring 2021")
 co6 = Cohort("Fall 2021")
 
 
-#db.session.add_all([t1, t2, t3, t4, n1, n2, c1, c2, c3, c4, c5, c6, c7, c8, c9,
+db.session.add_all([t1, t2, t3, t4, n1, n2, c1, c2, c3, c4, c5, c6, c7, c8, c9,
                  #  c10, c11, c12, c13, c14, i1, i2, i3, i4, i5, i6, i7, i8, i9,
                  #  i10, co1, co2, co3, co4, co5, co6, w1, w2, w3, w4, w5, w6,
                  #  w7, w8, w9])
-#db.session.commit()
+db.session.commit()
